48b247ba8b00b74269bd964a19e35e4f9a3f129e
rafael-aog <rafael.ao.guedes@gmail.com>
2023-07-17 21:31:32 -0300

    labeling

--- PDI/5/labeling.cpp
+++ PDI/5/labeling.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 int main(int argc, char** argv) {
   cv::Mat image, realce;
   int width, height;
-  int nobjects;
+  int nobjects, undentify;
 
   cv::Point p;
   image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
@@ -24,10 +24,36 @@ int main(int argc, char** argv) {
   p.y = 0;
 
   // busca objetos presentes
+  undentify = 0;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      if(i == 0 || j == 0 || i == height-1 || j == width-1) {
+        if (image.at<uchar>(i, j) == 255) {
+          // achou um objeto na borda
+          undentify++;
+          // para o floodfill as coordenadas
+          // x e y são trocadas.
+          p.x = j;
+          p.y = i;
+          // preenche o objeto com o contador
+          cv::floodFill(image, p, 0);
+        }
+      }
+    }
+  }
+  cv::imshow("image2", image);
+  cv::imwrite("bordaslisas.png", image);
+
+  p.x = 0;
+  p.y = 0;
+
+  cv::floodFill(image, p, 155);
+
+  // busca objetos presentes
   nobjects = 0;
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
-      if (image.at<uchar>(i, j) == 255) {
+      if (image.at<uchar>(i, j) == 0) {
         // achou um objeto
         nobjects++;
         // para o floodfill as coordenadas
@@ -35,10 +61,12 @@ int main(int argc, char** argv) {
         p.x = j;
         p.y = i;
         // preenche o objeto com o contador
-        cv::floodFill(image, p, nobjects);
+        cv::floodFill(image, p, 255);
       }
     }
   }
+
+  std::cout << "a figura tinha " << undentify << " objetos nas bordas\n";
   std::cout << "a figura tem " << nobjects << " bolhas\n";
   cv::imshow("image", image);
   cv::imwrite("labeling.png", image);
